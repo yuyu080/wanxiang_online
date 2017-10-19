3795444e949f3604fe779bb6f50bbed8490b406c
feng <fengqiang@bbdservice.com>
2017-10-19 15:41:11 +0800

    integrate all the module

--- src/main/scala/com/bbd/bigdata/WanxiangSinkToNeo4j.scala
+++ src/main/scala/com/bbd/bigdata/WanxiangSinkToNeo4j.scala
@@ -1,6 +1,15 @@
 package com.bbd.bigdata
 
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction
+import org.neo4j.driver.v1._
+import org.neo4j.driver.v1.Config
+import java.util.concurrent.TimeUnit._
+import com.bbd.bigdata.core.CypherToNeo4j
+import java.util.Properties
+import kafka.producer.ProducerConfig
+import kafka.producer.Producer
+import kafka.producer.KeyedMessage
 
 object WanxiangSinkToNeo4j {
   /*
@@ -8,8 +17,82 @@ object WanxiangSinkToNeo4j {
   * 主要重写三个方法open(),invoke(),close()
   * */
 
-  class WanxiangSinkToNeo4j extends RichSinkFunction{
+  class WanxiangSinkToNeo4j extends RichSinkFunction[String]{
+    private var driver : Driver = null
 
-    override def invoke(in: Nothing): Unit = ???
+    override def open(parameters: Configuration): Unit = {
+      /**
+        * open方法是初始化方法，会在invoke方法之前执行，执行一次。
+        */
+      //neo4j 连接信息
+      val conn_addr = "bolt://10.28.52.151:7687"
+      val user = "neo4j"
+      val passwd = "123456"
+      //加载驱动
+      driver = GraphDatabase.driver(conn_addr, AuthTokens.basic(user, passwd),Config.build().withMaxTransactionRetryTime( 15, SECONDS ).toConfig())
+    }
+
+    override def invoke(in: String): Unit = {
+      /**
+        * invoke()方法通过json信息，获取相应cypher，并插入到数据库中。
+        * in 输入的数据
+        * Exception processing
+        */
+      //一个tuple接收数据，包含（table_name,List<cypher>）
+      val tuple_cypher_message = CypherToNeo4j.getCypher(in)
+      val cypher_list = tuple_cypher_message._2
+
+      tuple_cypher_message._2(0) match {
+        case "message_error" => put_kafka_topic(in)
+        case _ => driver.session().writeTransaction(new TransactionWork[Integer]() {
+          override def execute(tx: Transaction): Option[Integer] = createRelation(tx,cypher_list)
+        })
+      }
+
+
+    }
+
+    def put_kafka_topic(message : String): Unit ={
+      /*
+      * 异常数据处理
+      * 接收消息为message_error时将相应message加入到异常队列
+      * 等候下一步处理
+      * */
+      try {
+        //读取配置文件，加载配置信息
+        val props = new Properties()
+        props.load(ClassLoader.getSystemResourceAsStream("kafka-producer.properties"))
+        val config = new ProducerConfig(props)
+        val producer = new Producer[String, String](config)
+        //put message to kafka topic
+        val message = new KeyedMessage[String, String]("wanxiang_exception_20171019", message)
+        producer.send(message)
+      } catch {
+        case e: Exception =>
+          e.printStackTrace()
+      }
+    }
+
+    def createRelation(tx: Transaction, cypher_list: Array[String]): Option[Integer] = {
+      /*接收cypher list 后，解析cypher
+      * 所有cypher包含在一个事务里
+      * 顺序执行
+      * */
+      try{
+        cypher_list.foreach(tx.run(_))
+      }catch {
+        case e: Exception => e.printStackTrace();Some(1)
+      }
+      None
+    }
+
+    override def close() {
+      if(driver != null){
+        driver.close()
+      }
+      super.close();
+    }
+
   }
+
 }
--- src/main/scala/com/bbd/bigdata/WanxiangStreaming.scala
+++ src/main/scala/com/bbd/bigdata/WanxiangStreaming.scala
@@ -1,59 +1,48 @@
 package com.bbd.bigdata
 
 import java.util.Properties
+import com.bbd.bigdata.WanxiangSinkToNeo4j.WanxiangSinkToNeo4j
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.streaming.api.CheckpointingMode
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
-
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
-
 import org.apache.flink.streaming.api.scala._
 
 
 object WanxiangStreaming {
-  def cypher_generate(message : String) : Array[String] = {
-
-    return null;
-  }
-  def cypher_fetch(message : String )  {
-    /*
-    *调用函数，输入一条数据，返回包含cypher语句或者数据异常信息
-    * if cypher: 直接连接neo4j执行
-    * if Error message: 写到异常数据kafka topic 等待后续处理
-    * */
-
-
-
-  }
   def main(args: Array[String]) {
-    /*entrance,define and init streaming execution environment configuration */
+    /*
+    * 程序入口，定义streaming执行环境的参数
+    * 定义source，sink相关配置，以及整个流的处理过程
+    * 自定义sink
+    * */
+    //环境参数定义
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.enableCheckpointing(100)
     env.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
     env.setRestartStrategy(RestartStrategies.fallBackRestart())
+
     //flink exactly_once
     env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
 
     //定义kafka 配置,KAFKA_BROKER和消费组
-    //val ZOOKEEPER_HOST = "10.28.40.11:2181,10.28.40.12:2181,10.28.40.13:2181,10.28.40.14:2181,10.28.40.15:2181"   //zk的地址，下面的是kafka的地址
     val KAFKA_BROKER = "10.28.40.11:9092,10.28.40.12:9092,10.28.40.13:9092,10.28.40.14:9092,10.28.40.15:9092"
     val TRANSACTION_GROUP = "bbd_wanxiang_20171010"
+
     //初始化kafka
     val kafkaProps = new Properties()
     //kafkaProps.load(ClassLoader.getSystemResourceAsStream("consumer.properties"))
-    //kafkaProps.setProperty("zookeeper.connect", ZOOKEEPER_HOST)
     kafkaProps.setProperty("bootstrap.servers", KAFKA_BROKER)
     kafkaProps.setProperty("group.id", TRANSACTION_GROUP)
-    //kafkaProps.setProperty("auto.offset.reset","earliest")
-    //env.getConfig.addDefaultKryoSerializer(unmodColl, UnmodifiableCollectionsSerializer.class)
 
-    val text = env.addSource(
+    //添加source
+    val streamingMessage = env.addSource(
       new FlinkKafkaConsumer010[String](("wanxiang_canal_20170919"), new SimpleStringSchema(), kafkaProps)).map(
       x => x.trim()
       )
-    //streamingMessage.addSink()
-    text.print()
-    env.execute("Wanxiang streaming data process")
+    streamingMessage.addSink(new WanxiangSinkToNeo4j())
+
+    env.execute("Wanxiang streaming data processing")
   }
 
 }
