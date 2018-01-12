1735a792f8d418b5b3b177fe36e8c5b969c4faec
yuyu080 <zhaoyunfeng@bbdservice.com>
2018-01-12 16:50:52 +0800

    加锁

--- src/main/scala/com/bbd/bigdata/core/DeleteOperate.scala
+++ src/main/scala/com/bbd/bigdata/core/DeleteOperate.scala
@@ -22,7 +22,9 @@ object DeleteOperate{
   def clearGdxx(bbd_qyxx_id: String): Array[String] = {
     val str1 =
       s"""
-         MATCH (p)-[:INVEST]->(r:Role:Invest)-[:INVEST]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
+         |MATCH (p)-[:INVEST]->(r:Role:Invest)-[:INVEST]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
+         |SET p.update_time = timestamp()
+         |SET c.update_time = timestamp()
          |WITH p,r,c
          |MATCH (p)-[:INVEST]->(r)-[:INVEST]->(c)
          |SET c.gdxx = 0
@@ -31,7 +33,7 @@ object DeleteOperate{
        """.stripMargin
     val str2 =
       s"""
-         MATCH (p)-[:VIRTUAL]->(r:Role)-[:VIRTUAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
+         |MATCH (p)-[:VIRTUAL]->(r:Role)-[:VIRTUAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
          |WHERE NOT (p)-[:LEGAL|SUPERVISOR|EXECUTIVE|DIRECTOR|BRANCH]->(:Entity:Role)-[:LEGAL|SUPERVISOR|EXECUTIVE|DIRECTOR|BRANCH]->(c)
          |SET p.update_time = timestamp()
          |SET c.update_time = timestamp()
@@ -43,7 +45,7 @@ object DeleteOperate{
   def clearBasic(bbd_qyxx_id: String): Array[String] = {
     val str =
       s"""
-         MATCH (p)-[:LEGAL]->(r:Role)-[:LEGAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
+         |MATCH (p)-[:LEGAL]->(r:Role)-[:LEGAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
          |SET p.update_time = timestamp()
          |SET c.update_time = timestamp()
          |WITH p,r,c
