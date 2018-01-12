f79f989d6392cdaa4a25a6b34c9447c0b0cc4da8
Ezio <huangweizhe@bbdservice.com>
2018-01-12 15:49:17 +0800

    判断是否删除VIRTUAL边

--- src/main/scala/com/bbd/bigdata/core/DeleteOperate.scala
+++ src/main/scala/com/bbd/bigdata/core/DeleteOperate.scala
@@ -4,14 +4,17 @@ object DeleteOperate{
   def clearBaxx(bbd_qyxx_id: String): Array[String] = {
     val str =
       s"""
-         |MATCH (p)-[:SUPERVISOR|EXECUTIVE|DIRECTOR|VIRTUAL]->(r:Role)-[:SUPERVISOR|EXECUTIVE|DIRECTOR|VIRTUAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
+         |MATCH (p)-[:SUPERVISOR|EXECUTIVE|DIRECTOR]->(r:Role)-[:SUPERVISOR|EXECUTIVE|DIRECTOR]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
          |SET p.update_time = timestamp()
-         |SET r.update_time = timestamp()
          |SET c.update_time = timestamp()
          |WITH p,r,c
-         |MATCH (p)-[:SUPERVISOR|EXECUTIVE|DIRECTOR|VIRTUAL]->(r)-[:SUPERVISOR|EXECUTIVE|DIRECTOR|VIRTUAL]->(c)
+         |MATCH (p)-[:SUPERVISOR|EXECUTIVE|DIRECTOR]->(r)-[:SUPERVISOR|EXECUTIVE|DIRECTOR]->(c)
          |SET c.baxx=0
          |DETACH DELETE r
+         |WITH p,c
+         |MATCH (p)-[:VIRTUAL]->(rr:Role)-[:VIRTUAL]->(c)
+         |WHERE NOT (p)-[:LEGAL|INVEST|BRANCH]->(:Entity:Role)-[:LEGAL|INVEST|BRANCH]->(c)
+         |DETACH DELETE rr
        """.stripMargin
     Array(str)
   }
@@ -19,10 +22,7 @@ object DeleteOperate{
   def clearGdxx(bbd_qyxx_id: String): Array[String] = {
     val str1 =
       s"""
-         |MATCH (p)-[:INVEST]->(r:Role:Invest)-[:INVEST]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
-         |SET p.update_time = timestamp()
-         |SET r.update_time = timestamp()
-         |SET c.update_time = timestamp()
+         MATCH (p)-[:INVEST]->(r:Role:Invest)-[:INVEST]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
          |WITH p,r,c
          |MATCH (p)-[:INVEST]->(r)-[:INVEST]->(c)
          |SET c.gdxx = 0
@@ -31,12 +31,10 @@ object DeleteOperate{
        """.stripMargin
     val str2 =
       s"""
-         |MATCH (p)-[:VIRTUAL]->(r:Role)-[:VIRTUAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
+         MATCH (p)-[:VIRTUAL]->(r:Role)-[:VIRTUAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
+         |WHERE NOT (p)-[:LEGAL|SUPERVISOR|EXECUTIVE|DIRECTOR|BRANCH]->(:Entity:Role)-[:LEGAL|SUPERVISOR|EXECUTIVE|DIRECTOR|BRANCH]->(c)
          |SET p.update_time = timestamp()
-         |SET r.update_time = timestamp()
          |SET c.update_time = timestamp()
-         |WITH p,r,c
-         |MATCH (r)
          |DETACH DELETE r
        """.stripMargin
     Array(str1, str2)
@@ -45,13 +43,16 @@ object DeleteOperate{
   def clearBasic(bbd_qyxx_id: String): Array[String] = {
     val str =
       s"""
-         |MATCH (p)-[:LEGAL|VIRTUAL]->(r:Role)-[:LEGAL|VIRTUAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
+         MATCH (p)-[:LEGAL]->(r:Role)-[:LEGAL]->(c:Company {bbd_qyxx_id:"$bbd_qyxx_id"})
          |SET p.update_time = timestamp()
-         |SET r.update_time = timestamp()
          |SET c.update_time = timestamp()
          |WITH p,r,c
          |MATCH (r)
          |DETACH DELETE r
+         |WITH p,c
+         |MATCH (p)-[:VIRTUAL]->(rr:Role)-[:VIRTUAL]->(c)
+         |WHERE NOT (p)-[:SUPERVISOR|EXECUTIVE|DIRECTOR|BRANCH|INVEST]->(:Entity:Role)-[:SUPERVISOR|EXECUTIVE|DIRECTOR|BRANCH|INVEST]->(c)
+         |DETACH DELETE rr
        """.stripMargin
     Array(str)
   }
